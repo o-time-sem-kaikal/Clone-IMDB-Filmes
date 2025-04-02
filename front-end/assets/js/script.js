c1b158701310f4794faa976e6d4908f4970c8d54
isaac451-andrade <166961938+isaac451-andrade@users.noreply.github.com>
2025-04-02 00:54:44 -0300

    Loading Page added

--- front-end/assets/js/script.js
+++ front-end/assets/js/script.js
@@ -9,18 +9,103 @@
 
 
 async function loadMovieAndTvContentFromAPI(){
-    const response = await fetch("http://127.0.0.1:8000/getMoviesAndTvContent/");
 
+    document.querySelector("#loading").style.display = "block";
+    document.querySelector("#backdrop-content").style.display = "block";
+
+    const response = await fetch("http://127.0.0.1:8000/getMoviesAndTvContent/");
     const responseObj = await response.json();
 
+    document.querySelector("#loading").style.display = "none";
+    document.querySelector("#backdrop-content").style.display = "none";
     // Código para carregar o content dos .movies ou .tv no console
-
+    console.log("Filmes:")
     responseObj.movies.forEach(movie => {
         console.log("Título do Filme:", movie.title);
         console.log("URL da Imagem:", movie.imgURL);
         console.log("Avaliação: ", movie.rate);
-        console.log("")
+        console.log("------------------------------");
+    })
+
+    console.log("==================================");
+
+    console.log("Séries:")
+    responseObj.tvShows.forEach(show => {
+        console.log("Título da Série:", show.title);
+        console.log("URL da Imagem:", show.imgURL);
+        console.log("Avaliação: ", show.rate);
+        console.log("------------------------------");
+    })
+
+    return responseObj;
+
+}
+
+
+async function sendContentToIndex(){
+    const dados = await loadMovieAndTvContentFromAPI();
+
+    dados.movies.forEach(movie => {
+
+        const divSlide = document.createElement("div");
+        divSlide.classList.add("slide");
+
+        const imgSlide = document.createElement("img");
+        imgSlide.classList.add("img-slide");
+        imgSlide.alt = `Imagem do filme ${movie.title}`;
+        imgSlide.src = movie.imgURL;
+
+        const rateText = document.createElement("p");
+        rateText.classList.add("slide-rate");
+
+        rateText.innerHTML = `<i class="bi bi-star-fill"></i><span>${movie.rate}</span><span class="star-span"><i class="bi bi-star"></i></span>`;
+
+        const movieTitle = document.createElement("h1");
+        movieTitle.classList.add("slide-movie-title");
+        movieTitle.textContent = movie.title;
+
+        const btnPlus = document.createElement("button");
+        btnPlus.classList.add("slide-btn-list");
+        btnPlus.innerHTML = '<i class="bi bi-plus"></i>Lista';
+
+
+        const btnTrailer = document.createElement("button");
+        btnTrailer.classList.add("slide-btn-trailer");
+
+        btnTrailer.innerHTML = '<i class="bi bi-caret-right-fill"></i>Trailer';
+
+        divSlide.appendChild(imgSlide);
+        divSlide.appendChild(rateText);
+        divSlide.appendChild(movieTitle);
+        divSlide.appendChild(btnPlus);
+        divSlide.appendChild(btnTrailer);
+
+        document.querySelector(".slider").appendChild(divSlide);
     })
 }
 
-loadMovieAndTvContentFromAPI();
+document.addEventListener("DOMContentLoaded", sendContentToIndex);
+
+// // url para imagens e nomes
+// const url = 'https://imdb8.p.rapidapi.com/actors/v2/get-images?nconst=nm0974575&first=1';
+
+// //url para born today
+
+// const url2 = 'https://imdb8.p.rapidapi.com/actors/v2/get-born-today?today=03-15&first=20&country=US&language=en-US';
+
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': '46c9b8bf22msh06c5ae5c58a4ad1p1d48edjsnb27791c7bbb3',
+// 		'x-rapidapi-host': 'imdb8.p.rapidapi.com'
+// 	}
+// };
+
+// fetch(url, options)
+// .then(response => response.json())
+// .then(data => {
+//     console.log(data)
+// })
+// .catch(error =>{
+//     console.error(error);
+// })
